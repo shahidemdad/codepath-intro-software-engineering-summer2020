05efcff51c5d3ef74af8c8997d64bb9117fe57f4
shahidemdad <shahidbinemdad@gmail.com>
2020-06-15 22:19:00 -0400

    Completed

--- week2/task5.cpp
+++ week2/task5.cpp
@@ -1,7 +1,8 @@
 /*
-Given a non-empty array of integers, every element appears twice except for one.
-Find that single one integer and return it.
-Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Task:
+    Given a non-empty array of integers, every element appears twice except for one.
+    Find that single one integer and return it.
+    Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
 
 #include <bits/stdc++.h>
