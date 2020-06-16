3e81b593ace41906c79efb69e5994be2c30b8371
shahidemdad <shahidbinemdad@gmail.com>
2020-06-15 22:21:53 -0400

    Submitted

---
+++ week2/task2.cpp
@@ -0,0 +1,4 @@
+/*
+Task:
+Given an array, rotate the array to the right by k steps, where k is non-negative. 
+Return the resulting array.*/
---
+++ week2/task4.cpp
@@ -0,0 +1,4 @@
+/*
+Task:
+    Merge Overlapping Intervals (Sorted)
+    Given a collection of intervals, merge all overlapping intervals and return the result.*/
